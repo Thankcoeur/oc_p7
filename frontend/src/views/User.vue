0ca033a8bd49e41794cb2a964b9822b90a760b49
maxime fedou-forgeard <maxime.fedou@gmail.com>
2021-05-22 14:47:23 +0200

    securite plus plus

--- frontend/src/views/User.vue
+++ frontend/src/views/User.vue
@@ -1,157 +1,104 @@
 <template>
   <div class="user">
+    <Compte
+      
+      :compte="compte"
+      v-for="compte in comptes"
+      v-bind:key="compte.id"
+      :deleteAcount="deleteAcount"
+    />
 
-
-
-
-
-
-<div class="content">
-
-
-<Compte v-for="compte in comptes" :compte="compte" v-bind:key="compte.id"  :deleteAcount="deleteAcount"  />
-
-
-
-</div>
-
-    
+    <Compte v-if="compte !== null" :compte="compte" :deleteAcount="deleteAcount" />
   </div>
 </template>
 
-<script> 
+<script>
 import axios from "axios";
 import { mapState } from "vuex";
-import Compte from "./../components/Compte"
-//import store from '../store';
+import Compte from "./../components/Compte";
+import store from "../store";
 export default {
   name: "User",
   data() {
     return {
-      compte : null,
-      comptes :  null
-      
+      compte: null,
+      comptes: null,
     };
   },
   computed: {
-    ...mapState(["user","getUser"])
+    ...mapState(["user", "getUser"]),
   },
-  components : {
-      Compte
-    
+  components: {
+    Compte,
   },
   methods: {
-   
     deleteAcount(id) {
-     
       axios
-        .delete("http://localhost:3000/api/user/delete/"+id, {
+        .delete("http://localhost:3000/api/user/delete/" + id, {
           headers: {
-            Authorization: "Bearer " + localStorage.getItem("token")
-          }
+            Authorization: "Bearer " + localStorage.getItem("token"),
+          },
         })
         .then(() => {
-          localStorage.clear()
-          this.$router.replace("/login")
-          
-          
+          localStorage.clear();
+          this.$router.replace("/login");
         })
-        .catch(error => console.log(error));
+        .catch((error) => console.log(error));
     },
-
- 
   },
   mounted() {
-    
-
-    
-
-
-      
-
-        axios
-        .get("http://localhost:3000/api/user/getUsers", {
-           headers: {
-            Authorization: "Bearer " +localStorage.getItem("token")
-          }
-        })
-        .then((user ) => {
-          this.comptes = user.data
-          
-          
-          
-        })
-        .catch(error => console.log(error));
-
-        
-
-
-
-         
-      
-
-
+    console.log(store.getters.getUser.isAdmin);
+    if (store.getters.getUser.isAdmin) {
       axios
-        .get("http://localhost:3000/api/user/getUser/"+localStorage.getItem("id"), {
-           headers: {
-            Authorization: "Bearer " +localStorage.getItem("token")
-          }
+        .get("http://localhost:3000/api/user/getUsers", {
+          headers: {
+            Authorization: "Bearer " + localStorage.getItem("token"),
+          },
         })
-        .then((user ) => {
-          this.compte = user.data
-          
-          
-          
+        .then((user) => {
+          console.log(user)
+          this.comptes = user.data;
         })
-        .catch(error => console.log(error));
+        .catch((error) => console.log(error));
+        return
     }
-  
 
-
-
-
-
-
-
-    
-
-}
-    
+    axios
+      .get(
+        "http://localhost:3000/api/user/getUser/" + localStorage.getItem("id"),
+        {
+          headers: {
+            Authorization: "Bearer " + localStorage.getItem("token"),
+          },
+        }
+      )
+      .then((user) => {
+        this.compte = user.data;
+      })
+      .catch((error) => console.log(error));
+  },
+};
 </script>
 
 <style lang="scss" scoped>
-
 body {
-
-  background-color:white;
+  background-color: white;
 }
 
 .content {
-
-margin-top : 150px;
-
+  margin-top: 150px;
 }
 
-.user  {
-
-  max-width:900px;
-  padding : 50px ;
- background-color: white;
-
-
+.user {
+  max-width: 900px;
+  padding: 50px;
+  background-color: white;
 }
 h1 {
-
-font-size: 18px
+  font-size: 18px;
 }
 
 h2 {
-
-  font-size:16px
-}
-.btn {
-
-
-
+  font-size: 16px;
 }
 </style>

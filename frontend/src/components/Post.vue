906699148bc6cf93bc775144a1c6a75c5cdf24a6
maxime fedou-forgeard <maxime.fedou@gmail.com>
2021-05-22 17:53:59 +0200

    refonte design

--- frontend/src/components/Post.vue
+++ frontend/src/components/Post.vue
@@ -2,7 +2,7 @@
   <div class="post">
     <div class="top">
       <div class="image-profil gradient-box "><img class="img-profil" src="https://www.gabrielgorgi.com/wp-content/uploads/2019/12/01.jpg.webp" alt=""></div>
-      <h2></h2>
+      <h2>{{user.username}}</h2>
 
     </div>
     <div class="content">
@@ -28,6 +28,7 @@
 </template>
 
 <script>
+import Axios from 'axios';
 import { mapState } from "vuex";
 import store from '../store';
 
@@ -35,19 +36,33 @@ export default {
   name: "Post",
   components: {},
   data() {
-    return {};
+    return {
+      user : {}
+    };
   },
   computed: {
     ...mapState(["user", "editOption"])
   },
-  props: {
-    post: {
-      type: Object,
-      required: true
-    }
-  },
+  props: ["post"], 
+ 
   mounted() {
+    
+   
 
+    Axios
+      .get(
+        "http://localhost:3000/api/user/getUser/"+ this.post.UserId,
+        {
+          headers: {
+            Authorization: "Bearer " + localStorage.getItem("token"),
+          },
+        }
+      )
+      .then((user) => {
+        console.log(user.data)
+        this.user = user.data;
+      })
+      .catch((error) => console.log(error));
 
   }
   ,
@@ -77,6 +92,7 @@ export default {
 <style lang="scss" scoped>
 
 .post {
+  color : rgb(44, 44, 44) ; 
   display: grid;
   grid-template-areas:
   
@@ -96,7 +112,12 @@ export default {
   margin-right: 10px;
 }
 .top {
+  font-size: 14px;
+  color: rgb(66, 66, 66);
   grid-area: top;
+  display: grid;
+  grid-template-columns: auto auto;
+  align-items: center;
   padding : 10px
   
 }
@@ -118,7 +139,10 @@ export default {
 
 .content {
   grid-area: content;
-  padding : 10px
+  padding : 10px;
+  p {
+    margin-bottom: 10px;
+  }
 }
 
 .image-profil {

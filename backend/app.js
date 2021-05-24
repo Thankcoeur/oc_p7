00d0af31a20bb047a1c2ce9515e82cadfb4d731a
maxime fedou-forgeard <maxime.fedou@gmail.com>
2021-05-24 12:17:54 +0200

    vf

--- backend/app.js
+++ backend/app.js
@@ -4,23 +4,30 @@ const bodyParser = require('body-parser');
 const cors  = require('cors');
 const userRoutes = require('./routes/user.js');
 const postRoutes = require('./routes/post.js');
+const rateLimit = require("express-rate-limit");
+
+
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000, 
+  max: 100 
+});
+
 
 const path = require('path');
 
-//création application Express
+//création app express
 const app = express();
 
 
 //Résolution erreur CORS
 app.use(cors())
 
-//Parser les corps des requête + forcer parse d'objets inclus dans d'autres objets
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 //Middleware
 app.use('/images', express.static(path.join(__dirname, 'images')));
-
+app.use(limiter);
 app.use('/api/user', userRoutes);
 app.use('/api/post', postRoutes);
 
--- backend/routes/user.js
+++ backend/routes/user.js
@@ -3,11 +3,17 @@ const router = express.Router();
 const userCtrl = require('../controllers/user');
 const auth = require('../middleware/auth');
 const  AcountAcces = require('../middleware/AcountAcces')
+const isAdmin = require('../middleware/isAdmin')
+var ExpressBrute = require('express-brute');
+ 
+var store = new ExpressBrute.MemoryStore(); // stores state locally, don't use this in production
+var bruteforce = new ExpressBrute(store);
 
-//Route des utilisateurs
-router.post("/signup", userCtrl.signup);
-router.post("/login", userCtrl.login);
-router.get('/getUsers', auth, userCtrl.getUsers);
+
+
+router.post("/signup",bruteforce.prevent, userCtrl.signup);
+router.post("/login",bruteforce.prevent, userCtrl.login);
+router.get('/getUsers', auth,isAdmin , userCtrl.getUsers);
 router.delete('/delete/:id', auth,AcountAcces, userCtrl.delete)
 router.get('/getUser/:id',auth,AcountAcces, userCtrl.getUser)
 module.exports = router;
